7b9775eeb809271740f893c8409650831b97c6da
leiyangs <15135210985@163.com>
2020-08-18 22:49:37 +0800

    restful routers

--- cms-api/config/config.default.js
+++ cms-api/config/config.default.js
@@ -2,30 +2,20 @@
 
 'use strict';
 
-/**
- * @param {Egg.EggAppInfo} appInfo app info
- */
 module.exports = appInfo => {
-  /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
   const config = exports = {};
 
-  // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1597755939854_6253';
 
-  // add your middleware config here
   config.middleware = [];
 
-  // add your user config here
   const userConfig = {
     mysql: {
       client: {
         host: 'localhost',
         port: '3306',
         user: 'root',
-        password: 'root',
+        password: '123456',
         database: 'cms',
       },
     },
