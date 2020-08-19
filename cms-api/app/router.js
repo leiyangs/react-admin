719583e047e4ad0ba75b3b5c688a2f008e07dd46
leiyangs <15135210985@163.com>
2020-08-19 22:34:34 +0800

    resources相当于多个route

--- cms-api/app/router.js
+++ cms-api/app/router.js
@@ -3,5 +3,6 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  // 这种写法一行顶多行 相当于分开写合起来 router.get('/api/user', controller.user.index); router.post('/api/user', controller.user.create);
   router.resources('user', '/api/user', controller.user);
 };
