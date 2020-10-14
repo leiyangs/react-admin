b3771babab5fd6b9565ef5c7924a1ae29cdf0632
leiyangs <15135210985@163.com>
2020-10-14 14:39:41 +0800

    FormWrapper styled

--- cms-front/README.md
+++ cms-front/README.md
@@ -1,7 +1,15 @@
 # 全栈cms
 
-`yarn create umi`
+`yarn create umi` umi生成的项目可选择包含dva和antd-design
 
 ```npm
 cnpm i styled-components jsonwebtoken -S
+```
+
+styled-components用来写组建的css
+
+```js
+const Wrapper = styled.div`
+    color: blue;
+  `;
 ```
--- cms-front/src/pages/login/index.js
+++ cms-front/src/pages/login/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, Input, Form } from 'antd';
+import 'antd/dist/antd.css';
+import styles from './index.css';
 import styled from 'styled-components';
-import { connect } from 'dva';
+import { connect } from 'dva'; // react-redux用来连接仓库和组件
 
 const { Content } = Layout;
 
@@ -9,8 +11,8 @@ class Login extends Component {
   render() {
     return (
       <>
-        <Layout>
-          <Content>
+        <Layout className={styles.layout}>
+          <Content className={styles.content}>
             <LoginForm/>
           </Content>
         </Layout>
@@ -23,11 +25,34 @@ class LoginForm extends Component {
   render() {
     return (
       <div>
-        aa
+        <FormWrapper>
+          <Form>
+            <Input placeholder="aaa"/>
+          </Form>
+        </FormWrapper>
       </div>
     )
   }
 }
+
+const FormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  algin-items: center;
+  // height: calc(100vh - 70px);
+  height: 100%;
+  margin: 120px auto;
+  h3 {
+    text-align: center;
+  }
+  form {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5%;
+    background: rgb(255, 255, 255);
+    padding: 35px;
+  }
+`;
 
 // 一个页面路由会对应一个子状态 login
 export default connect(
