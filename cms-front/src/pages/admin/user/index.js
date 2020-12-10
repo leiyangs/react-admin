d8e9304760f4bb57d5284fd3ad10e50564939b92
leiyangs <15135210985@163.com>
2020-12-10 14:18:12 +0800

    user Table

--- cms-front/src/pages/admin/user/index.js
+++ cms-front/src/pages/admin/user/index.js
@@ -9,32 +9,58 @@ export default
 @connect(state => state.user) // 装饰器 和connect()()一样，把class传入
 class User extends React.Component {
   render() {
-    console.log(this.props.list)
-    const dataSource = [
-      {
-        key: '1',
-        name: '胡彦斌',
-        age: 32,
-        address: '西湖区湖底公园1号',
+    const { list, total, pageNum } = this.props;
+    const pagination = {
+      current: pageNum,
+      total,
+      pageSize: PAGE_SIZE,
+      defaultPageSize: 3,
+      defaultCurrent: 1,
+      showSizeChanger: true, // 每页显示几条快捷
+      showQuickJumper: true, // 快速跳转到哪页
+      showTotal: (total, range) => {
+        return `共计${total}条 当前${range[0]}-${range[1]}`
       },
-      {
-        key: '2',
-        name: '胡彦祖',
-        age: 42,
-        address: '西湖区湖底公园1号',
+      onChange: (pageNum, pageSize) => {
+        // this.props.dispatch(routerRedux.push({
+        //   pathname: '/admin/user',
+        //   query: pageNum
+        // }))
+        this.props.dispatch(routerRedux.push(`/admin/user?pageNum=${pageNum}`))
       },
-    ];
+      onShowSizeChange: (current, size) => {
+        console.log(current,size)
+      }
+    }
     
     const columns = [
       {
         title: '姓名',
-        dataIndex: 'name',
-        key: 'name',
+        dataIndex: 'username',
+        key: 'username',
       },
       {
-        title: '年龄',
-        dataIndex: 'age',
-        key: 'age',
+        title: '生日',
+        dataIndex: 'birthday',
+        key: 'birthday',
+      },
+      {
+        title: '电话',
+        dataIndex: 'phone',
+        key: 'phone',
+      },
+      {
+        title: '邮箱',
+        dataIndex: 'email',
+        key: 'email',
+      },
+      {
+        title: '性别',
+        dataIndex: 'gender',
+        key: 'gender',
+        render: gender => {
+          return gender === 0 ? '男' : '女'
+        }
       },
       {
         title: '住址',
@@ -43,7 +69,8 @@ class User extends React.Component {
       },
     ];
     return (
-      <Table dataSource={dataSource} columns={columns} />
+      // 使用 rowKey 来指定 dataSource 的主键。若没有指定，控制台会出现报错的提示 `Each child in a list should have a unique "key" prop`
+      <Table rowKey="id" dataSource={list} columns={columns} pagination={pagination} />
     )
   }
 }
