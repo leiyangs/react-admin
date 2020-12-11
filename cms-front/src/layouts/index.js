8115234ef5df92a24330e6e755a7d1584cbb2aac
leiyangs <15135210985@163.com>
2020-12-11 10:50:29 +0800

    antd设置中文

--- cms-front/src/layouts/index.js
+++ cms-front/src/layouts/index.js
@@ -1,10 +1,12 @@
 // import styles from './index.css';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN';
 
 function BasicLayout(props) {
   return (
-    <div>
+    <ConfigProvider locale={zhCN}>
       {props.children}
-    </div>
+    </ConfigProvider>
   );
 }
 
